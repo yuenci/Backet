fc079ef5ddce0ce0c6e74c47837e84c13d6e283f
Innis <1575270674@qq.com>
2023-03-11 15:14:04 +0800

    feat: in progress card and complete card

--- CardTest.Designer.cs
+++ CardTest.Designer.cs
@@ -29,24 +29,118 @@
         private void InitializeComponent()
         {
             this.taskCard1 = new Backet.UI_Conponents.TaskCard();
+            this.uiPanel1 = new Sunny.UI.UIPanel();
+            this.TaskName = new Sunny.UI.UILabel();
+            this.IconsPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.uiLabel2 = new Sunny.UI.UILabel();
+            this.uiAvatar1 = new Sunny.UI.UIAvatar();
+            this.uiSymbolButton1 = new Sunny.UI.UISymbolButton();
+            this.uiPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // taskCard1
             // 
             this.taskCard1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(251)))), ((int)(((byte)(252)))));
-            this.taskCard1.Location = new System.Drawing.Point(241, 92);
+            this.taskCard1.Location = new System.Drawing.Point(29, 80);
             this.taskCard1.Name = "taskCard1";
             this.taskCard1.Size = new System.Drawing.Size(290, 240);
             this.taskCard1.TabIndex = 0;
+            // 
+            // uiPanel1
+            // 
+            this.uiPanel1.Controls.Add(this.uiSymbolButton1);
+            this.uiPanel1.Controls.Add(this.uiLabel2);
+            this.uiPanel1.Controls.Add(this.uiAvatar1);
+            this.uiPanel1.Controls.Add(this.TaskName);
+            this.uiPanel1.Controls.Add(this.IconsPanel);
+            this.uiPanel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(251)))), ((int)(((byte)(252)))));
+            this.uiPanel1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uiPanel1.Location = new System.Drawing.Point(395, 90);
+            this.uiPanel1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.uiPanel1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiPanel1.Name = "uiPanel1";
+            this.uiPanel1.Radius = 10;
+            this.uiPanel1.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(232)))), ((int)(((byte)(231)))));
+            this.uiPanel1.RectSize = 2;
+            this.uiPanel1.Size = new System.Drawing.Size(280, 230);
+            this.uiPanel1.Style = Sunny.UI.UIStyle.Custom;
+            this.uiPanel1.TabIndex = 3;
+            this.uiPanel1.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TaskName
+            // 
+            this.TaskName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TaskName.Location = new System.Drawing.Point(12, 67);
+            this.TaskName.Name = "TaskName";
+            this.TaskName.Size = new System.Drawing.Size(253, 45);
+            this.TaskName.Style = Sunny.UI.UIStyle.Custom;
+            this.TaskName.TabIndex = 1;
+            this.TaskName.Text = "uiLabel1uiLabel1uiLabel1uiLabel1uiLabel1";
+            this.TaskName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // IconsPanel
+            // 
+            this.IconsPanel.Location = new System.Drawing.Point(16, 13);
+            this.IconsPanel.Name = "IconsPanel";
+            this.IconsPanel.Size = new System.Drawing.Size(180, 35);
+            this.IconsPanel.TabIndex = 0;
+            // 
+            // uiLabel2
+            // 
+            this.uiLabel2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uiLabel2.Location = new System.Drawing.Point(48, 138);
+            this.uiLabel2.Name = "uiLabel2";
+            this.uiLabel2.Size = new System.Drawing.Size(201, 23);
+            this.uiLabel2.Style = Sunny.UI.UIStyle.Custom;
+            this.uiLabel2.TabIndex = 8;
+            this.uiLabel2.Text = "End: Nov 12";
+            this.uiLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // uiAvatar1
+            // 
+            this.uiAvatar1.AvatarSize = 25;
+            this.uiAvatar1.BackColor = System.Drawing.Color.Transparent;
+            this.uiAvatar1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiAvatar1.Location = new System.Drawing.Point(12, 136);
+            this.uiAvatar1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiAvatar1.Name = "uiAvatar1";
+            this.uiAvatar1.Size = new System.Drawing.Size(30, 30);
+            this.uiAvatar1.Style = Sunny.UI.UIStyle.Custom;
+            this.uiAvatar1.Symbol = 57589;
+            this.uiAvatar1.SymbolSize = 25;
+            this.uiAvatar1.TabIndex = 7;
+            this.uiAvatar1.Text = "uiAvatar1";
+            // 
+            // uiSymbolButton1
+            // 
+            this.uiSymbolButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiSymbolButton1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(243)))), ((int)(((byte)(213)))));
+            this.uiSymbolButton1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uiSymbolButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(153)))), ((int)(((byte)(56)))));
+            this.uiSymbolButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.uiSymbolButton1.Location = new System.Drawing.Point(16, 185);
+            this.uiSymbolButton1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiSymbolButton1.Name = "uiSymbolButton1";
+            this.uiSymbolButton1.Radius = 10;
+            this.uiSymbolButton1.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(243)))), ((int)(((byte)(213)))));
+            this.uiSymbolButton1.Size = new System.Drawing.Size(131, 25);
+            this.uiSymbolButton1.Style = Sunny.UI.UIStyle.Custom;
+            this.uiSymbolButton1.Symbol = 78;
+            this.uiSymbolButton1.SymbolColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(153)))), ((int)(((byte)(56)))));
+            this.uiSymbolButton1.TabIndex = 4;
+            this.uiSymbolButton1.Text = "Completed";
+            this.uiSymbolButton1.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             // 
             // CardTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.uiPanel1);
             this.Controls.Add(this.taskCard1);
             this.Name = "CardTest";
             this.Text = "CardTest";
+            this.uiPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -54,5 +148,11 @@
         #endregion
 
         private UI_Conponents.TaskCard taskCard1;
+        private Sunny.UI.UIPanel uiPanel1;
+        private Sunny.UI.UISymbolButton uiSymbolButton1;
+        private Sunny.UI.UILabel uiLabel2;
+        private Sunny.UI.UIAvatar uiAvatar1;
+        private Sunny.UI.UILabel TaskName;
+        private System.Windows.Forms.FlowLayoutPanel IconsPanel;
     }
 }
--- UI_Conponents/TaskCard.Designer.cs
+++ UI_Conponents/TaskCard.Designer.cs
@@ -30,11 +30,21 @@
         {
             this.uiPanel1 = new Sunny.UI.UIPanel();
             this.IconsPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.TaskName = new Sunny.UI.UILabel();
+            this.uiAvatar2 = new Sunny.UI.UIAvatar();
+            this.uiLabel1 = new Sunny.UI.UILabel();
+            this.ProcessBar = new Sunny.UI.UIProcessBar();
+            this.uiLabel2 = new Sunny.UI.UILabel();
             this.uiPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // uiPanel1
             // 
+            this.uiPanel1.Controls.Add(this.uiLabel2);
+            this.uiPanel1.Controls.Add(this.ProcessBar);
+            this.uiPanel1.Controls.Add(this.uiLabel1);
+            this.uiPanel1.Controls.Add(this.uiAvatar2);
+            this.uiPanel1.Controls.Add(this.TaskName);
             this.uiPanel1.Controls.Add(this.IconsPanel);
             this.uiPanel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(251)))), ((int)(((byte)(252)))));
             this.uiPanel1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -54,8 +64,73 @@
             // 
             this.IconsPanel.Location = new System.Drawing.Point(16, 13);
             this.IconsPanel.Name = "IconsPanel";
-            this.IconsPanel.Size = new System.Drawing.Size(180, 50);
+            this.IconsPanel.Size = new System.Drawing.Size(180, 35);
             this.IconsPanel.TabIndex = 0;
+            // 
+            // TaskName
+            // 
+            this.TaskName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TaskName.Location = new System.Drawing.Point(12, 67);
+            this.TaskName.Name = "TaskName";
+            this.TaskName.Size = new System.Drawing.Size(253, 45);
+            this.TaskName.Style = Sunny.UI.UIStyle.Custom;
+            this.TaskName.TabIndex = 1;
+            this.TaskName.Text = "uiLabel1uiLabel1uiLabel1uiLabel1uiLabel1";
+            this.TaskName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // uiAvatar2
+            // 
+            this.uiAvatar2.AvatarSize = 25;
+            this.uiAvatar2.BackColor = System.Drawing.Color.Transparent;
+            this.uiAvatar2.FillColor = System.Drawing.Color.Transparent;
+            this.uiAvatar2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiAvatar2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.uiAvatar2.Location = new System.Drawing.Point(9, 124);
+            this.uiAvatar2.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiAvatar2.Name = "uiAvatar2";
+            this.uiAvatar2.Size = new System.Drawing.Size(30, 30);
+            this.uiAvatar2.Style = Sunny.UI.UIStyle.Custom;
+            this.uiAvatar2.Symbol = 57379;
+            this.uiAvatar2.SymbolSize = 25;
+            this.uiAvatar2.TabIndex = 5;
+            this.uiAvatar2.Text = "uiAvatar2";
+            // 
+            // uiLabel1
+            // 
+            this.uiLabel1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uiLabel1.Location = new System.Drawing.Point(45, 126);
+            this.uiLabel1.Name = "uiLabel1";
+            this.uiLabel1.Size = new System.Drawing.Size(201, 23);
+            this.uiLabel1.Style = Sunny.UI.UIStyle.Custom;
+            this.uiLabel1.TabIndex = 6;
+            this.uiLabel1.Text = "Start: Nov 12";
+            this.uiLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // ProcessBar
+            // 
+            this.ProcessBar.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(236)))), ((int)(((byte)(236)))));
+            this.ProcessBar.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ProcessBar.ForeColor = System.Drawing.Color.Blue;
+            this.ProcessBar.Location = new System.Drawing.Point(16, 169);
+            this.ProcessBar.MinimumSize = new System.Drawing.Size(70, 3);
+            this.ProcessBar.Name = "ProcessBar";
+            this.ProcessBar.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(152)))), ((int)(((byte)(55)))));
+            this.ProcessBar.Size = new System.Drawing.Size(249, 7);
+            this.ProcessBar.Style = Sunny.UI.UIStyle.Custom;
+            this.ProcessBar.TabIndex = 7;
+            this.ProcessBar.Text = "uiProcessBar1";
+            this.ProcessBar.Value = 50;
+            // 
+            // uiLabel2
+            // 
+            this.uiLabel2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uiLabel2.Location = new System.Drawing.Point(12, 191);
+            this.uiLabel2.Name = "uiLabel2";
+            this.uiLabel2.Size = new System.Drawing.Size(201, 23);
+            this.uiLabel2.Style = Sunny.UI.UIStyle.Custom;
+            this.uiLabel2.TabIndex = 8;
+            this.uiLabel2.Text = "Last updated: 10 Dec, 2022";
+            this.uiLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // TaskCard
             // 
@@ -74,5 +149,10 @@
 
         private Sunny.UI.UIPanel uiPanel1;
         private System.Windows.Forms.FlowLayoutPanel IconsPanel;
+        private Sunny.UI.UILabel TaskName;
+        private Sunny.UI.UILabel uiLabel2;
+        private Sunny.UI.UIProcessBar ProcessBar;
+        private Sunny.UI.UILabel uiLabel1;
+        private Sunny.UI.UIAvatar uiAvatar2;
     }
 }
