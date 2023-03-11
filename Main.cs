e23fb7e3fcff4bb769098b2eaf7f8a161e68ee9d
Innis <1575270674@qq.com>
2023-03-11 17:40:08 +0800

    feat: add githubapi

--- Main.cs
+++ Main.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,7 @@ namespace Backet
         {
             InitializeComponent();
             AddCard();
+            Tools.DetectDataFile();
         }
 
         private void SearchTextBox_Enter(object sender, EventArgs e)
@@ -53,6 +55,17 @@ namespace Backet
         {
             Submit submitForm = new Submit();
             submitForm.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            Console.WriteLine(currentDirectory);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Tools.TestGetRepoInfo();
         }
     }
 }
---
+++ Tools.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Backet
+{
+    public class Tools
+    {
+        static public string GetRunningPath()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            //Console.WriteLine(currentDirectory);
+            return currentDirectory;
+        }
+
+        static public void  DetectDataFile()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            
+            string dataFolderPath = Path.Combine(currentDirectory, "data");
+ 
+
+            if (!Directory.Exists(dataFolderPath))
+            {
+                Directory.CreateDirectory(dataFolderPath);
+                Console.WriteLine("Data Folder created");
+            }
+            else
+            {
+                Console.WriteLine("Data Folder exist");
+            }
+        }
+
+        public static async Task<string> GetRepoInfo(string apiUrl, string apiToken)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+                client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("C#", "1.0"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", apiToken);
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    return responseBody;
+                }
+                else
+                {
+                    throw new Exception("Failed to retrieve repository information");
+                }
+            }
+        }
+
+        public static async void  TestGetRepoInfo()
+        {
+            string apiUrl = URLtoAPIURL("https://github.com/yuenci/Java-Car-Rental-System");
+            ;
+            try
+            {
+                string repoInfo = await GetRepoInfo(apiUrl, Token.token);
+                WriteRepoJson("Java-Car-Rental-System", repoInfo);
+                Console.WriteLine(repoInfo);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static string URLtoAPIURL(string repoUrl)
+        {
+            Uri uri = new Uri(repoUrl);
+            string apiUrl = $"https://api.github.com/repos/{uri.Segments[1]}{uri.Segments[2].TrimEnd('/')}";
+            return apiUrl;
+        }
+
+
+        public static void WriteRepoJson(string repoName, string textContent)
+        {
+            string dataFolderPath = Path.Combine(GetRunningPath(), "data");
+            
+            string filePath = Path.Combine(dataFolderPath, $"{repoName}.json");
+            File.WriteAllText(filePath, textContent);
+        }
+    }
+}
